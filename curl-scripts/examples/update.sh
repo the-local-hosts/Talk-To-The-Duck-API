48c768060672066de7c85e322cd9b5bc472f2d15
hollyklose <hollyklose@gmail.com>
2019-06-26 15:33:19 -0400

    Create patch route and test with curl-script
    Pair program with @elizadavis @hollyklose

--- curl-scripts/examples/update.sh
+++ curl-scripts/examples/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+TOKEN="734a1703a9d09ac1761d91bbedf3cda9"
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/blogposts"
+TITLE="This title is so much better!"
+ID="5d13c681a42036e8e4e86ddd"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +11,8 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "blogpost": {
+      "title": "'"${TITLE}"'"
     }
   }'
 
