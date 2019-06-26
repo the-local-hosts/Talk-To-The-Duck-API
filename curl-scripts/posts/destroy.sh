6d739b6903d895ac66b058ed4a0b286e24bdf5b0
elizadavis <eelizadavis@gmail.com>
2019-06-26 15:02:18 -0400

    Write Curl Scripts
    Wrote and modified curl scripts for testing
    @hollyklose @elizadavis

--- curl-scripts/posts/destroy.sh
+++ curl-scripts/posts/destroy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/books"
-ID=5d0cf67ed2b65dd7790fd0f0
-TOKEN=ac74ac2eab01d7720a7fe59bfc8483df
+URL_PATH="/blogposts"
+ID="5d13b3950f6c020757dc3f2c"
+TOKEN="dcef98931e5babd3a9cffebcac9730a4"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
