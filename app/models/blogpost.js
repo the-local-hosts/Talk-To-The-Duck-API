6f5ffb5b8c4c1516cace1c399d9a3dfef23b5ba5
hollyklose <hollyklose@gmail.com>
2019-06-27 08:52:30 -0400

    Comment out Editable in Blogpost model
    Plan to handle editable test on the front end.

--- app/models/blogpost.js
+++ app/models/blogpost.js
@@ -23,14 +23,14 @@ const blogPostSchema = new mongoose.Schema({
 }
 )
 
-blogPostSchema.virtual('editable?').get(function (req) {
-  // Need to compare this to currently logged in user's id to see if
-  // the owner of the blog post is the currently logged in user.
-  if (this.owner._id) {
-    return true
-  } else {
-    return false
-  }
-})
+// blogPostSchema.virtual('editable?').get(function () {
+//   // Need to compare this to currently logged in user's id to see if
+//   // the owner of the blog post is the currently logged in user.
+//   if (this.owner._id) {
+//     return true
+//   } else {
+//     return false
+//   }
+// })
 
 module.exports = mongoose.model('BlogPost', blogPostSchema)
