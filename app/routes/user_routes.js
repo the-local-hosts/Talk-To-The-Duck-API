273868b37420b0a65386145785fae2d193e54641
kevinrosario <kevin.rosario9@upr.edu>
2019-06-30 23:15:18 -0400

    Fixed follow issue

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -153,19 +153,24 @@ router.patch('/follow/:_id', requireToken, (req, res, next) => {
     .then(userToFollow => {
       return userToFollow.update({'$push': { 'following': req.params._id }}) // push user that started following
     })
-    .then(() => res.sendStatus(204))
+    .then(() => {
+      User.findById(req.user.id)
+        .then(handle404)
+        .then(user => res.status(200).json({ user: user.toObject() }))
+        .catch(next)
+    })
     .catch(next)
 })
 
-router.get('/users', (req, res, next) => {
-  User.find()
-    .populate('following', 'name')
-    .populate('followers', 'name')
-    .then(users => {
-      return users.map(user => user.toObject())
-    })
-    .then(users => res.status(200).json({ users: users }))
-    .catch(next)
-})
+// router.get('/users', (req, res, next) => {
+//   User.find()
+//     .populate('following', 'name')
+//     .populate('followers', 'name')
+//     .then(users => {
+//       return users.map(user => user.toObject())
+//     })
+//     .then(users => res.status(200).json({ users: users }))
+//     .catch(next)
+// })
 
 module.exports = router
